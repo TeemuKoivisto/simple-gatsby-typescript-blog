f70531e76f7ef835f86d31328e4fe3fbdce2fbf2
Teemu Koivisto <tkoivisto456@gmail.com>
2018-10-27 22:56:34 +0300

    Add LICENSE, update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,13 @@
 
 # Copy the build into bucket with 30 day cache-control and delete all existing files
 aws s3 sync ./public s3://teemukoivisto.xyz/ \
+  --region eu-west-1 \
+  --acl public-read \
   --cache-control max-age=2592000 \
   --delete
 
 # Copy index.html the second time with decreased cache duration
 aws s3 cp ./public/index.html s3://teemukoivisto.xyz/ \
+  --region eu-west-1 \
+  --acl public-read \
   --cache-control max-age=120
