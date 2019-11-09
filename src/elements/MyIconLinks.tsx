16860487cb218cc1b95551660e21c215fbcb04a8
Teemu Koivisto <tkoivisto456@gmail.com>
2019-11-09 15:33:25 +0200

    Improve some accessibility and manifest configuraiton

--- src/elements/MyIconLinks.tsx
+++ src/elements/MyIconLinks.tsx
@@ -13,13 +13,30 @@ function MyIconLinksEl(props: IProps) {
   return (
     <Container className={props.className}>
       <Links>
-        <IconLink href="mailto:teemukoivisto.xyz@gmail.com" onClick={outboundClick}>
+        <IconLink
+          href="mailto:teemukoivisto.xyz@gmail.com"
+          onClick={outboundClick}
+          rel="noopener"
+          aria-label="Send an email"
+        >
           <IoMdMail size={24}/>
         </IconLink>
-        <IconLink href="https://github.com/teemukoivisto" onClick={outboundClick} target="_blank">
+        <IconLink
+          href="https://github.com/teemukoivisto"
+          onClick={outboundClick}
+          target="_blank"
+          rel="noopener"
+          aria-label="My Github profile"
+        >
           <IoLogoGithub size={24}/>
         </IconLink>
-        <IconLink href="https://www.linkedin.com/in/teemu-koivisto-75304b114" onClick={outboundClick} target="_blank">
+        <IconLink
+          href="https://www.linkedin.com/in/teemu-koivisto-75304b114"
+          onClick={outboundClick}
+          target="_blank"
+          rel="noopener"
+          aria-label="My Linkedin profile"
+        >
           <IoLogoLinkedin size={24}/>
         </IconLink>
       </Links>
@@ -33,8 +50,12 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
 `
-const EmailText = styled.small`
+const EmailText = styled.address`
   color: #fff;
+  font-size: 0.8rem;
+  font-style: normal;
+  margin: 0;
+  padding: 0;
 `
 const Links = styled.div`
   display: flex;
